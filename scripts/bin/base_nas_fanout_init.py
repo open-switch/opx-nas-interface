78168e4e69fc99ca635dea9962d453a886d1e94c
nchervela <37422187+nchervela@users.noreply.github.com>
2018-11-05 11:12:43 -0800

    feat: remove intermediate bash script (#58)
    * feat: delete intermediate shell script
    
    Signed-off-by: Nalini Chervela <nalini_chervela@dell.com>

--- scripts/bin/base_nas_fanout_init.py
+++ scripts/bin/base_nas_fanout_init.py
@@ -18,11 +18,13 @@ from xml.dom import minidom
 import event_log as ev
 from time import sleep
 import os
+import sys
 import subprocess
 import cps
 import nas_os_if_utils as nas_if
 
 fanout_config_file = "/etc/opx/dn_nas_fanout_init_config.xml"
+nocreate_file = "/etc/opx/nas_if_nocreate"
 
 def parse_intf_config(intf_list):
     """parse the fanout config from xml and
@@ -59,6 +61,9 @@ def parse_config_file():
 
 
 if __name__ == '__main__':
+
+    if os.path.isfile(nocreate_file):
+        sys.exit(0) 
 
     # Wait till interface service is ready
     while nas_if.nas_os_if_list() == None:
