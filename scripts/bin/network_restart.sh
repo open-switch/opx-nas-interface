50cae2fb1b671203ca06512a4c27efea2877e962
Garrick He <32883830+GarrickHe@users.noreply.github.com>
2018-11-28 09:50:19 -0800

    fix: Interface settings not programmed into NPU (#67)
    * Bugfix: Bring down all interfaces (excluding system loopback) before Networking
              service restarts to ensure all interface settings in /etc/network/interfaces
              are properly programmed into the NPU via NetLink messages
    * Update: Correct debian/changelog version number
    
    Signed-off-by: Garrick He <garrick_he@dell.com>

--- scripts/bin/network_restart.sh
+++ scripts/bin/network_restart.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-# Restart networking service without shuting down
-# interfaces that are already up
+# Bring down all interfaces (except system loopback) before restarting Networking service
+# to ensure that all interface settings gets programmed into the NPU
+
+/sbin/ifdown -a --exclude=lo
 service networking restart
