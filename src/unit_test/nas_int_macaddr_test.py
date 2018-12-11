d74d10c297c5e10fd5b5506403a713cc10026516
Garrick He <32883830+GarrickHe@users.noreply.github.com>
2018-12-11 10:28:14 -0800

    fix: Update copyright information (#71)
    * Update: Copyright information
    
    Signed-off-by: Garrick He <garrick_he@dell.com>

--- src/unit_test/nas_int_macaddr_test.py
+++ src/unit_test/nas_int_macaddr_test.py
@@ -1,3 +1,18 @@
+#
+# Copyright (c) 2018 Dell Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT
+# LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS
+# FOR A PARTICULAR PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT.
+#
+# See the Apache Version 2.0 License for specific language governing
+# permissions and limitations under the License.
+#
 from nas_mac_addr_utils import get_alloc_mac_addr_params, if_get_mac_addr
 import cps_utils
 import cps_object
